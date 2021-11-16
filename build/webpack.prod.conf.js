51705e4fed50fc02afac372bdc017d81f0c7bda6
yangyuji <394712273@qq.com>
2021-11-16 10:35:57 +0800

    修改打包配置

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -55,7 +55,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'h5-factory',
+      title: 'form-factory',
       templateParameters: {
         BASE_URL: config.build.assetsPublicPath + config.build.assetsSubDirectory,
       },
